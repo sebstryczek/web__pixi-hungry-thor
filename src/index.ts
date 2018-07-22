7b5f63d184956a7fe5856b0ac590d2725738d9e8
Sebastian <sebastian@stryczek.pl>
2018-07-22 16:48:32 +0200

    Add PIXI

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
-interface TestInterface {
-  testString: string;
+// interface TestInterface {
+//   testString: string;
+// }
+
+// class TestClass implements TestInterface {
+//   testString: string;
+
+//   constructor(public s: string) {
+//     this.testString = s;
+//   }
+// }
+
+// function testFunction(s : TestInterface) {
+//   return `Test: ${s.testString}`;
+// }
+
+// const test = new TestClass("!@#");
+// document.body.innerHTML = testFunction(test);
+
+import Game from './Game';
+
+const game = new Game();
+game.initialize();
+
+function elo() {
+  
 }
-
-class TestClass implements TestInterface {
-  testString: string;
-
-  constructor(public s: string) {
-    this.testString = s;
-  }
-}
-
-function testFunction(s : TestInterface) {
-  return `Test: ${s.testString}`;
-}
-
-const test = new TestClass("!@#");
-document.body.innerHTML = testFunction(test);
