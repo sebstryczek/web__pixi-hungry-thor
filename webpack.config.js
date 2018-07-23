0b94a51c1ae9dc6a9e6095c29e7018e4dc9d1755
Sebastian <sebastian@stryczek.pl>
2018-07-23 12:44:44 +0200

    Add images and configure webpack to use static assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const htmlWebpackPlugin = new HtmlWebPackPlugin({
   template: path.resolve(__dirname, 'src/index.html'),
   filename: 'index.html'
 });
+
+const copyWebpackPlugin = new CopyWebpackPlugin([
+  { from: path.resolve(__dirname, 'src/assets'), to: 'assets' }
+]);
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -23,14 +28,15 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: [ '.ts', '.js' ]
+    extensions: ['.ts', '.js']
   },
   output: {
+    publicPath: '/',
     path: path.resolve(__dirname, 'build'),
     filename: '[name].js' //'[name].[hash].js'
   },
   devServer: {
     port: 3000
   },
-  plugins: [htmlWebpackPlugin]
+  plugins: [htmlWebpackPlugin, copyWebpackPlugin]
 };
